d462242e5e450e451df8512b84f4c9a195d74c42
Pranavd500 <114133861+Pranavd500@users.noreply.github.com>
2024-06-28 23:55:42 +0530

    28th jun 2nd commit

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,119 @@
-import React from "react";
+"use client";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import Link from "next/link";
 
+import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Description } from "@radix-ui/react-dialog";
+
+const projects = [
+  {
+    num: "01",
+    category: "frontend",
+    title: "project",
+    description: "Lorem ipsumm Shit",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/assests/work/",
+    live: "",
+    github: "",
+  },
+  {
+    num: "01",
+    category: "frontend",
+    title: "project",
+    description: "Lorem ipsumm Shit",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/assests/work/",
+    live: "",
+    github: "",
+  },
+  {
+    num: "01",
+    category: "frontend",
+    title: "project",
+    description: "Lorem ipsumm Shit",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/assests/work/",
+    live: "",
+    github: "",
+  },
+  {
+    num: "01",
+    category: "frontend",
+    title: "project",
+    description: "Lorem ipsumm Shit",
+    stack: [{ name: "Html 5" }, { name: "Css 3" }, { name: "Javascript" }],
+    image: "/assests/work/",
+    live: "",
+    github: "",
+  },
+];
 const work = () => {
-  return <div>this is work</div>;
+  const [project, setProject] = useState(projects[0]);
+  return (
+    <motion.section
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
+    >
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row xl:gap-[30px]">
+          <div className="w-full xl:wl-[50% xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
+            <div>
+              <div className="text-8xl leading-none fon-extrabold text-transparent text-outline">
+                {project.num}
+              </div>
+              <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500 capitalize">
+                {project.category} Project
+              </h2>
+              <p className="text-white/60">{project.description}</p>
+              <ul className="flex gap-4">
+                {project.stack.map((item, index) => {
+                  return (
+                    <li key={index} className="text-xl text-accent">
+                      {item.name}
+                      {index !== project.stack.length - 1 && ","}
+                    </li>
+                  );
+                })}
+              </ul>
+              <div className="border border-white/20"></div>
+              <div className="flex items-center gap-4"></div>
+              <Link href={project.live}>
+                <TooltipProvider delayDuration={100}>
+                  <Tooltip>
+                    <TooltipTrigger>
+                      <BsArrowUpRight />
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>Live Project</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </Link>
+              <Link href={project.github}></Link>
+              <TooltipProvider delayDuration={100}>
+                <Tooltip>
+                  <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                    <BsGithub className="text-white text-3xl" />
+                  </TooltipTrigger>
+                </Tooltip>
+              </TooltipProvider>
+            </div>
+          </div>
+          <div className="w-full xl:w-[50%]"> slider </div>
+        </div>
+      </div>
+    </motion.section>
+  );
 };
 
 export default work;
