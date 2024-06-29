e1322f8460cdf1d7b2c4d219d6405ec40877711a
Pranavd500 <114133861+Pranavd500@users.noreply.github.com>
2024-06-29 20:17:09 +0530

    contact oage last hurray

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -13,6 +13,7 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { Description } from "@radix-ui/react-dialog";
+import WorkSliderBtns from "@/components/WorkSliderBtns";
 
 const projects = [
   {
@@ -26,7 +27,7 @@ const projects = [
     github: "",
   },
   {
-    num: "01",
+    num: "02",
     category: "frontend",
     title: "project",
     description: "Lorem ipsumm Shit",
@@ -36,7 +37,7 @@ const projects = [
     github: "",
   },
   {
-    num: "01",
+    num: "03",
     category: "frontend",
     title: "project",
     description: "Lorem ipsumm Shit",
@@ -58,16 +59,24 @@ const projects = [
 ];
 const work = () => {
   const [project, setProject] = useState(projects[0]);
+  const handleSlideChange = (swiper) => {
+    const currentIndex = swiper.activeIndex;
+    setProject(projects[currentIndex]);
+  };
+
   return (
     <motion.section
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={{
+        opacity: 1,
+        transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+      }}
       className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
     >
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row xl:gap-[30px]">
           <div className="w-full xl:wl-[50% xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
-            <div>
+            <div className="flex flex-col gap-[30px] h-[50%]">
               <div className="text-8xl leading-none fon-extrabold text-transparent text-outline">
                 {project.num}
               </div>
@@ -109,7 +118,29 @@ const work = () => {
               </TooltipProvider>
             </div>
           </div>
-          <div className="w-full xl:w-[50%]"> slider </div>
+          <div className="w-full xl:w-[50%]">
+            <Swiper
+              spaceBetween={30}
+              slidesPerView={1}
+              className="xl:h-[520px] mb-12"
+              onSlideChange={handleSlideChange}
+            >
+              {projects.map((project, index) => {
+                return (
+                  <SwiperSlide key={index} className="w-full">
+                    <div className="h-[460px] relative group flex justfiy -center items-center bg-pink-50">
+                      <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
+                      <div className="relative w-full h-full"></div>
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+              <WorkSliderBtns
+                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+              />
+            </Swiper>
+          </div>
         </div>
       </div>
     </motion.section>
